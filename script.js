91da6b42dafefb8abdd695dae690dd34e7ecb4af
Josh Jacob Sunil <joshjacobsunil@gmail.com>
2026-08-28 22:25:59 +0530

    Insert frame 150 from fourth mesh as the 5th step

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const scrollyContainer = document.getElementById('scrolly-container');
-    const totalSteps = 7; // Steps 0 to 7 (8 total)
+    const totalSteps = 8; // Steps 0 to 8 (9 total)
     
     function updateScrollState() {
         const rect = scrollyContainer.getBoundingClientRect();
